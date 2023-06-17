1aab22362e88855e542ced117dbd02f1897c38d5
Mhassan799 <93198093+Mhassan799@users.noreply.github.com>
2023-06-18 02:23:35 +0500

    Add files via upload

---
+++ routes/userRoutes.js
@@ -0,0 +1,10 @@
+
+const {createUser,login,getUsers} = require('../controller/userController')
+const express = require('express')
+const router = express.Router()
+const {verifyToken} = require('../middleware/jwtMiddleware')
+router.post('/createUser',createUser)
+router.post('/login',login)
+router.get('/getUsers',getUsers)
+
+module.exports = router
